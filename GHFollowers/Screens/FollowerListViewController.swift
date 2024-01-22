f8ffd33ac93f0ce8340a407db21a6cbab12a5c51
LMuchapirei <h170407h@hit.ac.zw>
2024-01-22 11:17:51 +0200

    Add Result at call site of API Call

--- GHFollowers/Screens/FollowerListViewController.swift
+++ GHFollowers/Screens/FollowerListViewController.swift
@@ -16,9 +16,11 @@ class FollowerListViewController: UIViewController {
         
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             switch result {
+                
             case .success(let followers):
                 print("Followers.count = \(followers.count)")
                 print(followers)
+                
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff", message: error.rawValue, buttonTitle: "OK")
             }
