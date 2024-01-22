e60ea4823a0b94ac2422f6fa0d12b369843da982
LMuchapirei <h170407h@hit.ac.zw>
2024-01-22 10:46:22 +0200

    Using the completion handler with optionals

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -15,34 +15,34 @@ class NetworkManager {
         
     }
     
-    func getFollowers(for userName: String , page: Int,completed: @escaping (Result<[Follower],GFError>) -> Void) {
+    func getFollowers(for userName: String , page: Int,completed: @escaping ([Follower]?,GFError?) -> Void) {
         let endpoint = baseURL + "\(userName)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completed(.failure(GFError.invalidUserName))
+            completed(nil,GFError.invalidUserName)
             return
         }
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let _ = error {
-                completed(.failure(.unableToComplete))
+                completed(nil,.unableToComplete)
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(.failure(.invalidResponse))
+                completed(nil,.invalidResponse)
                 return
             }
             
             guard let data = data else {
-                completed(.failure(.invalidData))
+                completed(nil,.invalidData)
                 return
             }
             do {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
-                completed(.success(followers))
+                completed(followers,nil)
             } catch {
-                completed(.failure(.invalidData))
+                completed(nil,.invalidData)
             }
             
         }
