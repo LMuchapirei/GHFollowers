01c48b6a69fa66b0831fea841d0dd4c7a27c998f
LMuchapirei <muchapireil@gmail.com>
2024-01-29 12:01:29 +0200

    Create extension method on view to show,an empty state for the case no data

--- GHFollowers/Screens/FollowerListViewController.swift
+++ GHFollowers/Screens/FollowerListViewController.swift
@@ -58,6 +58,11 @@ class FollowerListViewController: UIViewController {
                 print("Followers.count = \(followers.count)")
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Do follow them."
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view)}
+                    return
+                }
                 self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff", message: error.rawValue, buttonTitle: "OK")
