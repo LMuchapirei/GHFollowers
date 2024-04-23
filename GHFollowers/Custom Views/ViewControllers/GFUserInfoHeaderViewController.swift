2364a0b68a12d857b37671d39c33ea1310f3f6e5
LMuchapirei <muchapireil@gmail.com>
2024-04-23 16:06:36 +0200

    Layout constraints for the header view

--- GHFollowers/Custom Views/ViewControllers/GFUserInfoHeaderViewController.swift
+++ GHFollowers/Custom Views/ViewControllers/GFUserInfoHeaderViewController.swift
@@ -49,6 +49,32 @@ class GFUserInfoHeaderViewController: UIViewController {
         locationImageView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: view.topAnchor,constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 90),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 90),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor,constant: textImagePadding),
+            usernameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 38),
+            
+            nameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor,constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor,constant: textImagePadding),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -padding),
+            nameLabel.heightAnchor.constraint(equalToConstant: 28),
+            
+            locationImageView.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor),
+            locationImageView.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor,constant: textImagePadding),
+            locationImageView.heightAnchor.constraint(equalToConstant: 20),
+            locationImageView.widthAnchor.constraint(equalToConstant: 20),
+            
+            locationLabel.centerYAnchor.constraint(equalTo: locationImageView.centerYAnchor),
+            locationLabel.leadingAnchor.constraint(equalTo: locationImageView.trailingAnchor,constant: 5),
+            locationLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -padding),
+            locationLabel.heightAnchor.constraint(equalToConstant: 20)
+            
+            
             
         ])
     }
