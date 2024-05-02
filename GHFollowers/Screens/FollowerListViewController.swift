577435ce5b9d98bf240d8a4419c60eea948fbec3
LMuchapirei <muchapireil@gmail.com>
2024-05-02 09:23:40 +0200

    Add logic to add favorites,to the add function in FollowerListViewController

--- GHFollowers/Screens/FollowerListViewController.swift
+++ GHFollowers/Screens/FollowerListViewController.swift
@@ -110,7 +110,26 @@ class FollowerListViewController: UIViewController {
     }
     
     @objc func  addButtonTapped(){
-        print("Add Button Tapped")
+        showLoadingView()
+        NetworkManager.shared.getUserInformation(for: username) {[weak self] result in
+            guard let self = self else { return }
+            self.dismissLoadingView()
+            switch result {
+                case .success(let user):
+                    let favorite = Follower(login: user.login, avatarUrl: user.avatarUrl)
+                PersistanceManager.updateWith(favorite: favorite, actionType: PersistanceActionType.add) {[weak self] error in
+                    guard let self = self else { return }
+                    guard let error = error else {
+                        self.presentGFAlertOnMainThread(title: "Success", message: "You have successfully favorited this user", buttonTitle: "Ok")
+                        return
+                    }
+                    self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+                }
+                    break
+                case .failure(let error):
+                    self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
 }
 
