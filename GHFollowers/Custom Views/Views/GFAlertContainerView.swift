313904577a1eb924dcfdc9d4dbe1fd5c1c4620ac
LMuchapirei <muchapireil@gmail.com>
2024-05-14 12:11:54 +0200

    Refactor alert

---
+++ GHFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,23 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by Linval Muchapirei on 14/5/2024.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+          fatalError("init(coder:) has not been implemented")
+   }
+    
+    private func cofigure(){
+                           m
+    }
+}
